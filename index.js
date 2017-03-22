23cd44693e942828be6962c4db3ebfe5886755ac
thadeuszlay <thadeuszlay@gmail.com>
2017-03-22 22:43:37 +0100

    change file

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var slackTerminal = require('slack-terminalize');
 var config = require('./config/config.json');
 
-slackTerminal.init(config.API_TOKEN, {
-    // slack rtm client options here
-    // more info at: https://github.com/slackhq/node-slack-client/blob/master/lib/clients/rtm/client.js
+slackTerminal.init(process.env.API_TOKEN, {
+  // slack rtm client options here
+  // more info at: https://github.com/slackhq/node-slack-client/blob/master/lib/clients/rtm/client.js
 }, {
-    // app configurations to suit your project structure
-    // to see the list of all possible config,
-    // check this out: https://github.com/ggauravr/slack-terminalize/blob/master/util/config.js
-	CONFIG_DIR: __dirname + '/config',
-	COMMAND_DIR: __dirname + '/commands'
+  // app configurations to suit your project structure
+  // to see the list of all possible config,
+  // check this out: https://github.com/ggauravr/slack-terminalize/blob/master/util/config.js
+  CONFIG_DIR: __dirname + '/config',
+  COMMAND_DIR: __dirname + '/commands'
 });
